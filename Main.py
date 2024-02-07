0963729731bfa638d7b47e4cdf4d3ce96384244e
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-02-07 15:02:16 -0700

    made enemy behavior
    enemy is cracked at following player. you will lose. Can also have caffeine shakes

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
+import random
 
 # pygame setup
 pygame.init()
@@ -9,6 +10,8 @@ running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+enemy_pos = pygame.Vector2(screen.get_width() / 3, screen.get_height() / 3)
+
 
 while running:
     # poll for events
@@ -18,12 +21,12 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    screen.fill("green")
+    pygame.draw.circle(screen, "red", enemy_pos, 50)
+    pygame.draw.circle(screen, "brown", player_pos, 40)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if keys[pygame.K_SPACE]:
         player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
         player_pos.y += 300 * dt
@@ -31,7 +34,18 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
-
+    if enemy_pos.y < player_pos.y:
+        enemy_pos.y += 200 * dt
+    else:
+        enemy_pos.y -= 200 * dt
+    if enemy_pos.x < player_pos.x:
+        enemy_pos.x += 200 * dt
+    else:
+        enemy_pos.x -= 200 * dt
+    if enemy_pos.y != screen.get_height:
+        enemy_pos.y += random.randint(-300,300)* dt
+    #if enemy_pos.x != screen.get_height:
+      #enemy_pos.x += random.randint(-300,300) * dt
     # flip() the display to put your work on screen
     pygame.display.flip()
 
