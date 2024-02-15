47aeeb584f685b6cb46a015408c16295f1017875
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-02-15 15:43:04 -0700

    added start menu
    I added the start menu and was able to put a stupid image. It can also be used as a pause menu

--- Main.py
+++ Main.py
@@ -4,20 +4,24 @@ import random
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-game_state = "game_run"
+game_state = "start_menu"
 keys = pygame.key.get_pressed()
 def draw_start_menu():
-    screen.fill((0,0,0))
-    font = pygame.font.SysFont('comic sans', 40)
-    title = font.render('Monke Game', True,(255,255,255))
-    start_button = font.render('Start', True,(255,255,255))
-    screen.blit(title, (screen.get_width()/2 - title.get_width()/2, screen.get_height/2 - title.get_height()/ 2))
-    screen.blit(start_button, (screen.get_width()/2 - title.get_width()/2, screen.get_height/2 - title.get_height()/ 2))
-    pygame.display.update()
+   screen.fill((0, 0, 0))
+   background = pygame.image.load("start_menu.png").convert()
+   font = pygame.font.SysFont('arial', 40)
+   title = font.render('Monke Game', True, (255, 255, 255))
+   start_button = font.render('Press Space to start game', True, (255, 255, 255))
+   screen.blit(background, (screen_width - background.get_width(), screen_height - background.get_height()))
+   screen.blit(title, (screen_width/2 - title.get_width()/2, screen_height/2 - title.get_height()/2))
+   screen.blit(start_button, (screen_width/2 - start_button.get_width()/2, screen_height/2 + start_button.get_height()/2))
+   pygame.display.update()
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 enemy_pos = pygame.Vector2(screen.get_width() / 3, screen.get_height() / 3)
@@ -30,12 +34,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    if game_state == "start_menu":
+        draw_start_menu()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            game_state = "game_run"
     # fill the screen with a color to wipe away anything from last frame
     if game_state == "game_run":
         screen.fill("green")
         pygame.draw.circle(screen, "red", enemy_pos, 50)
         pygame.draw.circle(screen, "brown", player_pos, 40)
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             player_pos.y -= 300 * dt
         if keys[pygame.K_s]:
@@ -56,10 +65,10 @@ while running:
             enemy_pos.y += random.randint(-300,300)* dt
         if enemy_pos.x != screen.get_height:
             enemy_pos.x += random.randint(-300,300) * dt
+        if keys[pygame.K_ESCAPE]:
+            game_state = "start_menu"
         # flip() the display to put your work on screen
-        pygame.display.flip()
-    elif game_state == "game_over":
-        2
+    pygame.display.update()
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
